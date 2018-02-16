59f500b007606c0b3ff1bc51160a42eb96a9a328
dsdanielsodre <dsdanielsodre@gmail.com>
2018-02-16 18:06:30 -0200

    Botoes, footer e carousel

--- dmassessoriaeconsultoria/index.php
+++ dmassessoriaeconsultoria/index.php
@@ -16,7 +16,7 @@
   <body>
 
       <header>
-        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark" style="red">
           <a class="navbar-brand" href="index.php">DM Assesoria e Consultoria</a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -46,7 +46,7 @@
 
       <main role="main">
 
-        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <div id="myCarousel" class="carousel slide" data-ride="carousel"  style="text-align:center">
           <ol class="carousel-indicators">
             <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
             <li data-target="#myCarousel" data-slide-to="1"></li>
@@ -54,12 +54,12 @@
           </ol>
           <div class="carousel-inner">
             <div class="carousel-item active">
-              <img class="first-slide" src="img/computador.jpg" alt="First slide">
+              <img class="first-slide" src="img/extintores.png" alt="First slide">
               <div class="container">
                 <div class="carousel-caption text-left">
-                  <h1>Example headline.</h1>
-                  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                  <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+                  <h1>Manutenção.</h1>
+                  <p>Agende sua manutenção de extintores hoje.</p>
+                  <p><a class="btn btn-lg btn-primary" href="#" role="button">Agende aqui</a></p>
                 </div>
               </div>
             </div>
@@ -107,7 +107,7 @@
               <img class="rounded-circle" src="img/valvula2.jpg" alt="Generic placeholder image" width="140" height="140" style="background-color: red;">
               <h2>Produtos</h2> <!-- PRODUTOS -->
               <p>A troca de peças se faz necessária devido ao desgaste físico das mesmas. </p>
-              <p><a class="btn btn-secondary" href="#" role="button">Detalhes &raquo;</a></p>
+              <p><a class="btn btn-secondary" href="produtos.php" role="button">Detalhes &raquo;</a></p>
             </div><!-- /.col-lg-4 -->
             <div class="col-lg-4">
               <img class="rounded-circle" src="img/extintor2.png" alt="Generic placeholder image" width="140" height="140">
@@ -115,13 +115,13 @@
               <p>  DM Assessoria e Consultoria vêm com o objetivo de conscientizar a importância da prevenção no combate a incêndio, 
                 pois muitas pessoas podem imaginar que o extintor é um equipamento ultrapassado. Porém, estatísticas mostram que
                 cerca de 90% dos incêndios  se desenvolvem a partir de minúsculos focos.</p>
-              <p><a class="btn btn-secondary" href="#" role="button">Detalhes &raquo;</a></p>
+              <p><a class="btn btn-secondary" href="manutencoes.php" role="button">Detalhes &raquo;</a></p>
             </div><!-- /.col-lg-4 -->
             <div class="col-lg-4">
               <img class="rounded-circle" src="img/nr23.png" alt="Generic placeholder image" width="140" height="140">
               <h2>NR23</h2>  <!-- NR23 -->
               <p>NR 23 Inspeção de extintores de incêndio.</p>
-              <p><a class="btn btn-secondary" href="#" role="button">Detalhes &raquo;</a></p>
+              <p><a class="btn btn-secondary" href="nr23.php" role="button">Detalhes &raquo;</a></p>
             </div><!-- /.col-lg-4 -->
           </div><!-- /.row -->
 
@@ -136,7 +136,7 @@
               <p class="lead">Donec ullamcorper nulla non metus auctor fringilla.
                 Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna,
                 vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-                <p><a class="btn btn-green btn-buy" href="#" role="button">Comprar &raquo;</a></p>
+                <p><a class="btn btn-green btn-buy" href="produtos.php" role="button">Comprar &raquo;</a></p>
             </div>
             <div class="col-md-5">
               <img class="featurette-image img-fluid mx-auto" src="img/valvula2.jpg" alt="Generic placeholder image">
@@ -151,6 +151,7 @@
             <div class="col-md-7 order-md-2">
               <h2 class="featurette-heading">Manutenções. <span class="text-muted">Extintores de Incêndio.</span></h2>
               <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+                <p><a class="btn btn-green btn-buy" href="manutencoes.php" role="button">Comprar &raquo;</a></p>
             </div>
             <div class="col-md-5 order-md-1">
               <img class="featurette-image img-fluid mx-auto" src="img/extintor.jpg" alt="Generic placeholder image">
@@ -163,6 +164,7 @@
             <div class="col-md-7">
               <h2 class="featurette-heading">NR23. <span class="text-muted">Segurança.</span></h2>
               <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+                <p><a class="btn btn-green btn-buy" href="nr23.php" role="button">Comprar &raquo;</a></p>
             </div>
             <div class="col-md-5">
               <img class="featurette-image img-fluid mx-auto" src="img/nr23.png" alt="Generic placeholder image">
@@ -171,7 +173,7 @@
 
           <hr class="featurette-divider">
 
-  <h2 class="featurette-heading">Solicite Aqui. <span class="text-muted">Mande sua Mensagem.</span></h2>
+  <h2 class="featurette-heading">Fale Conosco. <span class="text-muted">Mande sua Mensagem.</span></h2>
           </br>
 <form> <!--FORMS--->
   <div class="form-group"> <!-- NOME -->
@@ -203,13 +205,41 @@
 
         </div><!-- /.container -->
 
-
+  </main>
+  </body>
         <!-- FOOTER -->
-        <footer class="container">
+        <footer>
+
+          <div class="caixa">
+              <h2 class="featurette-heading">Sobre Nós. <span class="text-muted"></h2>
+            <p> Na Damiao Machado, nos dedicamos em fornecer a nossos clientes um serviço cordial. Nós valorizamos seu negócio. Tornamos nossa missão o fornecimento de produtos e serviços confiáveis em nossa abordagem voltada ao cliente.</p>
+
+<p>Nossa equipe cortês e profissional dispõe de uma variedade de serviços para atender suas necessidades.</p>
+
+<p>Para saber mais sobre a Damiao Machado e os produtos e serviços que oferecemos, ligue para 55-11-957938458. </p>
+
+          </div>
+
+          <div class="caixa">
+              <h2 class="featurette-heading">Localização. <span class="text-muted"></h2>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+             velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+          </div>
+
+          <div class="caixa">
+            <h2 class="featurette-heading">Fale Conosco. <span class="text-muted"></h2>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+          </div>
           <p class="float-right"><a href="#">Voltar ao topo</a></p>
           <p>&copy; 2018 DM Assesoria e Consultoria, Inc. &middot; <a href="#">Privacidade</a> &middot; <a href="#">Termos</a></p>
+          <p><a href="#">Fale Conosco.</a></p>
+          <p>Fone: 55-11-957938458. </p>
+          <p>Email: usuario@email.com. </p>
         </footer>
-      </main>
 
-  </body>
+
+
 </html>
