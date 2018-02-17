c65fc2a2948d1f3f09d76475896aa30615b5d9c2
dsdanielsodre <dsdanielsodre@gmail.com>
2018-02-17 18:57:42 -0200

    grid

---
+++ grid/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+
+    <style media="screen">
+      .customDiv  {
+        float: left;
+        position: relative;
+        width: 200px;
+        height: 200px;
+        background-color: #000;
+        color: #fff;
+        margin: 30px;
+      }
+    </style>
+
+  </head>
+  <body>
+
+ <div class="container">
+   <div class="row">
+     <div class="col-md-4">
+       <div class="customDiv">Column 1</div>
+     </div>
+
+     <div class="col-md-4">
+       <div class="customDiv">Column 2</div>
+     </div>
+
+     <div class="col-md-4">
+       <div class="customDiv">  Column 3</div>
+     </div>
+
+     <div class="col-md-4">
+       <div class="customDiv"> Column 4 </div>
+     </div>
+
+  </div>
+
+  </div>
+
+  </body>
+</html>
