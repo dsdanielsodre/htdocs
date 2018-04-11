2dd1c2a230a8f3dca845240010b82cf970866246
dsdanielsodre <dsdanielsodre@gmail.com>
2018-04-11 05:10:54 -0300

    sangue do meu sangue

---
+++ site/sanguedomeusangue/index.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <?php
+         include 'links.php';
+         include 'metas.php';
+         include 'scripts.php';
+         ?>
+      <!-- MAPS -->
+      <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBQAx6CLr0p1zQFDhB-5lHxn_8X49pkAxE&callback=initMap"></script>
+      <!-- MAPS -->
+      <title>Sangue do meu Sangue</title>
+   </head>
+   <body>
+      <?php
+         include "menu.php";
+
+         ?>
+      <main role="main">
+         <div class="container marketing">
+            <div class="row"  id="">
+               <div class="col-md-12">
+                  <div id="myCarousel" class="carousel slide" data-ride="carousel"  style="text-align:center">
+                     <ol class="carousel-indicators">
+                        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                        <li data-target="#myCarousel" data-slide-to="1"></li>
+                        <li data-target="#myCarousel" data-slide-to="2"></li>
+                     </ol>
+                     <div class="carousel-inner">
+                        <div class="carousel-item active">
+                           <img class="first-slide" src="img/nos.png" alt="Treinamento">
+                           <div class="container">
+                              <div class="carousel-caption text-left"  id="down">
+                                 <h1></h1>
+                                 <p></p>
+                                 <p><a class="btn btn-lg btn-primary" href="contato.html" role="button">Agende seu Treinamento</a></p>
+                              </div>
+                           </div>
+                        </div>
+                        <div class="carousel-item">
+                           <img class="second-slide" src="img/doacao.png" alt="Second slide">
+                           <div class="container">
+                              <div class="carousel-caption">
+                                 <h1></h1>
+                                 <p></p>
+                                 <p><a class="btn btn-lg btn-primary" href="#" role="button">Ler Mais</a></p>
+                              </div>
+                           </div>
+                        </div>
+                        <div class="carousel-item">
+                           <img class="third-slide" src="img/parceiros.png" alt="Third slide">
+                           <div class="container">
+                              <div class="carousel-caption text-right">
+                                 <h3>Venda e Recarga de extintores,
+
+                                 <p>mangueiras e equipamentos.</h3></p>
+                                 <p><a class="btn btn-lg btn-primary" href="#" role="button">Saiba mais</a></p>
+                              </div>
+                           </div>
+                        </div>
+                        <div class="carousel-item">
+                           <img class="third-slide" src="img/medula.png" alt="Third slide">
+                           <div class="container">
+                              <div class="carousel-caption text-right">
+                                 <h3>Venda e Recarga de extintores,
+
+                                 <p>mangueiras e equipamentos.</h3></p>
+                                 <p><a class="btn btn-lg btn-primary" href="#" role="button">Saiba mais</a></p>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                     <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                     <span class="sr-only">Previous</span>
+                     </a>
+                     <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                     <span class="sr-only">Next</span>
+                     </a>
+                  </div>
+               </div>
+            </div>
+
+            <!-- row-->
+            <!-- Marketing messaging and featurettes
+               ================================================== -->
+            <!-- Wrap the rest of the page in another container to center all the content. -->
+            </br>
+            <!-- Three columns of text below the carousel -->
+            <div class="row">
+              <div class="col-lg-12">
+                   <hr class="featurette-divider">
+              </div>
+
+               <div class="col-lg-3">
+
+                  <img class="rounded-circle" src="img/nos.png" alt="Generic placeholder image">
+                  <h3>QUEM
+                     <p>SOMOS</h3></p>
+                  <!-- PRODUTOS -->
+                  <p>
+                  <h6>A Sangue do meu Sangue incentiva milhares de pessoas a contribuirem com as doações de sangue
+				  em hospitais que possuem baixo estoque, através de palestras motivacionais e informativas,
+				  juntamente com o instituições parceiras.
+                  </h6>
+                  </p>
+                  <p><a class="btn btn-secondary" href="produtos.html" role="button">VER PRODUTOS &raquo;</a></p>
+               </div>
+               <!-- /.col-lg-4 -->
+               <div class="col-lg-3">
+                  <img class="rounded-circle" src="img/doacao.png" alt="Generic placeholder image">
+                  <h3>DOAÇÕES</h3>
+                  <!-- MANUTENÇÕES -->
+                  <p>
+                  <h6>Saiba como doar sangue, locais para doar, cuidados que devemos ter ao doar sangue, grandes instituições parceiras e
+				 como realizar doações em grupo.
+
+                  </h6>
+                  </p>
+                  <p><a class="btn btn-secondary" href="manutencoes.html" role="button">LEIA MAIS &raquo;</a></p>
+               </div>
+               <!-- /.col-lg-4 -->
+               <div class="col-lg-3">
+                  <img class="rounded-circle" src="img/parceiros.png" alt="Generic placeholder image">
+                  <h3>INSTITUIÇÕES</h3>
+                  <!-- nr23 -->
+                  <p>
+                  <h6>A Sangue do meu Sangue trabalha em prol da conscientização das pessoas
+				  sobre a doação de sangue, através da exemplificação de grandes instituições, como a Pro Sangue.
+                  </h6>
+                  </p>
+                  <p><a class="btn btn-secondary" href="nr23.html" role="button">SAIBA MAIS &raquo;</a></p>
+               </div>
+               <div class="col-lg-3">
+                  <img class="rounded-circle" src="img/medula.png" alt="Generic placeholder image">
+                  <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation
+                    </h3>
+                  <!-- nr23 -->
+                  <p>
+                  <h6>
+                  </h6>
+                  </p>
+                  <p><a class="btn btn-secondary" href="nr23.html" role="button">LEIA MAIS &raquo;</a></p>
+               </div>
+               <!-- /.row -->
+               <!-- START THE FEATURETTES -->
+            </div>
+            <hr class="featurette-divider">
+            <div class="row">
+               <div class="col-lg-6 col-sm-12 ">
+                  <h3>ENTRE EM CONTATO</h3>
+                  <!-- nr23 -->
+                  <p>Em caso de dúvidas,  entre em contato:</p>
+				     <p>
+                  <h6>Fone 1: <p>
+                    (11) X.XXXX-XXXX(TIM)</h6></p>
+
+                  <h6>Fone 2:<p>
+                    (11) X.XXXX-XXXX (nextel)</h6></p>
+                  <h6>ID: -</h6>
+                  </p>
+                  <p>
+                  <h6>Email: contato@sanguedomeusangue.com.br </h6>
+
+                  </p>
+                  <img src="img/coleta_sangue2.jpeg" alt="Generic placeholder image" class="foto" >
+               </div>
+
+
+               <div class="col-lg-6  col-sm-12 ">
+                   <hr class="featurette-divider">
+                  <h3>DATAS COMEMORATIVAS</h3>
+
+                  <!-- nr23 -->
+                  <p>Datas comemorativas de doação de sangue.</p>
+                  <div class="quadrado">
+                    14
+                    <br>
+                    JUN
+                  </div>
+                    <div class="calendario">
+                  </div>
+
+                  <div class="quadrado">
+                    03
+                    <br>
+                    JUL
+                  </div>
+                  <div class="quadrado">
+                    17
+                    <br>
+                    JUL
+                  </div>
+
+               </div>
+            </div>
+
+         </div>
+         <!--MARKETING-->
+         <!-- /.container -->
+         <!-- FOOTER -->
+         <footer>
+            <div class="row" id="caixa">
+               <div class="rodape col-sm-12 col-lg-5">
+                  <h2 class="featurette-heading golden">Contato<span class="text-muted"></h2>
+                  </p>
+                  <h5>
+                  Para saber mais sobre a Sangue do meu Sangue, e entre em contato conosco.
+                  <h5>
+                  </p>
+                  <br>
+                  <p>
+                  <h5>Fone 1: <p>
+                    (11) X.XXXX-XXXX(TIM)</h5></p>
+
+                  <h5>Fone 2:<p>
+                    (11) X.XXXX-XXXX (nextel)</h5></p>
+                  <h5>ID: -</h5>
+                  </p>
+                  <p>
+                  <h5>Email: contato@sanguedomeusangue.com.br </h5>
+
+                  </p>
+
+               </div>
+
+               <div class="rodape col-sm-12 col-lg-5">
+                  <h2 class="featurette-heading golden">Sobre Nós<span class="text-muted"></h2>
+                  <p>
+                  <h5> A Sangue do meu Sangue incentiva milhares de pessoas a contribuirem com as doações de sangue
+				  em hospitais que possuem baixo estoque, através de palestras motivacionais e informativas,
+				  juntamente com o instituições parceiras.
+                  </h5>
+                  </p>
+                  <p>
+                  <h5>
+                  </h5>
+                  </p>
+               </div>
+               <p class="float-right"><a href="#"><img src="img/topo.png" style="width:50px; height: 50px; position:absolute; float: right; margin: auto; right:10px; top:10px"></a></p>
+            </div>
+            <div class="copy"> &copy; 2018 Sangue do meu Sangue. &middot; <a href="#">Privacidade</a> &middot; <a href="#">Termos</a></div>
+         </footer>
+      </main>
+   </body>
+</html>
---
+++ site/sanguedomeusangue/menu.php
@@ -0,0 +1,50 @@
+﻿<header>
+
+       <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark" style="red">
+       <!--fixed-top-->
+      <!-- <a class="navbar-brand" href="index.php">
+          <img src="img/logodm.png" alt="DM Assessoria e Consultoria" style=" width: 150px; height: 60px;">
+        </a> -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+       <span class="navbar-toggler-icon"></span>
+
+     </button>
+       <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto" >
+             <a href="index.php" class="dropbtn" role="button" id="ativo">Sangue do <br>meu Sangue</a>
+             <div class="dropdown">
+                <button class="dropbtn">Quem Somos</button>
+                <div class="dropdown-content">
+                   <a href="historico.php">Historico</a>
+                   <a href="objetivo.php">Nosso Objetivo</a>
+                   <a href="missao.php">Nossa Missão</a>
+                </div>
+             </div>
+             <div class="dropdown">
+                <button class="dropbtn">Doações</button>
+                <div class="dropdown-content">
+                   <a href="requisitos_doacao.php">Requisitos para Doação</a>
+                   <a href="como_doar.php">Como Doar</a>
+                   <a href="quem_pode_doar.php">Quem Pode doar</a>
+                   <a href="cuidados_pos_doacao.php">Cuidados Pós Doação</a>
+                   <a href="postos_doacao.php">Postos de Doação</a>
+                   <a href="doacao_grupo.php">Doação em Grupo</a>
+				   <a href="medula_ossea.php">Doação de Medula Óssea</a>
+                <!--   <a href="porta_corta_fogo.php">Porta Corta Fogo</a>
+                   <a href="acessorios_porta_corta_fogo.php">Acessórios de Porta Corta Fogo</a> -->
+                </div>
+             </div>
+             <div class="dropdown">
+                <button class="dropbtn">Instituições</button>
+                <div class="dropdown-content">
+                   <a href="pro_sangue.php">Pro Sangue</a>
+                </div>
+             </div>
+
+             <li class="nav-item">
+              <a class="nav-link disabled" href="#" style="font-size:16px; margin-top:8px;">Fale Conosco</a>
+            </li>
+            <!-- <a href="contato.php" class="dropbtn disabled" role="button" id="contato">Fale Conosco</a>-->
+          </ul>
+       </div>
+</header>
